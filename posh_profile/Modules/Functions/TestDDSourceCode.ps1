783f47e8e41fbec85fd3bed36c7cefd19c6a8c8b
daxaker <itjustsoap@gmail.com>
2020-04-10 17:51:15 +0300

    update TestDDSourceCode

--- posh_profile/Modules/Functions/TestDDSourceCode.ps1
+++ posh_profile/Modules/Functions/TestDDSourceCode.ps1
@@ -1,5 +1,14 @@
-function TestDDSourceCode {
-    $testPath = "C:\Projects\ECAD\Main\Tests\DeltaDesign.Tests\bin\Debug"
-    Set-Location $testPath
-    vstest.console.exe $testPath\*.Tests.dll
+function TestDDSourceCode() {
+    Param(
+    [parameter(Mandatory=$false)]
+    [String]
+    $Build = "Build"
+    )
+    $testPath = "C:\Projects\ECAD\Main\Tests\DeltaDesign.Tests\"
+    
+    if ($Build -ne  "NoBuild"){
+        MSBuild.exe $testPath
+    }
+
+    vstest.console.exe $testPath\bin\Debug\*.Tests.dll
 }
