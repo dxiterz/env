7fd5b58019a75867f7ef9e041fa25c32a2022c5a
daxaker <itjustsoap@gmail.com>
2020-05-09 22:47:09 +0300

    add. 3d_printer configs and posh profile

--- posh_profile/Microsoft.PowerShell_profile.ps1
+++ posh_profile/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,12 @@
-$env:Path = $env:Path + ";C:\users\zubik\appdata\local\nuget;C:\users\zubik\appdata\local\nuget\nufetch"
+$env:Path = $env:Path + ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\" `
+    + ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\" `
+    + ";C:\Program Files\JetBrains\JetBrains Rider 2019.3.4\jbr\bin\"
+
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module Helpers
 Set-Theme Agnoster
+
+Set-Alias pbcopy Set-Clipboard
+Set-Alias pbpaste Get-Clipboard
+Set-Variable ddSource C:\Projects\ECAD\Main\Source\
