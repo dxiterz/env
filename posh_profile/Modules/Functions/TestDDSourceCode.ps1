a58fa0d81dd8a681d7c1890c8cd05afc5cde18ea
daxaker <itjustsoap@gmail.com>
2020-05-15 18:24:46 +0300

    fix TestDDSource

--- posh_profile/Modules/Functions/TestDDSourceCode.ps1
+++ posh_profile/Modules/Functions/TestDDSourceCode.ps1
@@ -3,11 +3,14 @@ function TestDDSourceCode() {
         [switch]$Build = $false
     )
 
+    $returnLocation = $PWD
     $testPath = "C:\Projects\ECAD\Main\Tests\DeltaDesign.Tests\"
-    
+
+    Set-Location $testPath
     if ($Build){
         MSBuild.exe $testPath
     }
 
     vstest.console.exe $testPath\bin\Debug\*.Tests.dll
+    Set-Location $returnLocation
 }
