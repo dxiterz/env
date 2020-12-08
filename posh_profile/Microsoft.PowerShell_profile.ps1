d6b6457ac3d7cf2442e2da600644dbd9349a0b8b
daxaker <itjustsoap@gmail.com>
2020-12-08 18:51:33 +0300

    add usr/bin

--- posh_profile/Microsoft.PowerShell_profile.ps1
+++ posh_profile/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,8 @@
 $env:Path = $env:Path + ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\" `
     + ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\" `
-    + ";C:\Program Files\JetBrains\JetBrains Rider 2019.3.4\jbr\bin\" `
-    + ";C:\Program Files\OpenSSL\bin\"
+    + ";C:\Program Files\JetBrains\JetBrains Rider 2020.1.4\jbr\bin\" `
+    + ";C:\Program Files\OpenSSL\bin\" `
+    + ";C:\Users\dx\bin\"
 
 Import-Module posh-git
 Import-Module oh-my-posh
