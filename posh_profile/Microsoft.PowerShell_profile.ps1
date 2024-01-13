4ba4dc269493e375ad54bbcac8afeb2a5de23d49
Zubik Dmitriy <zubik@eremex.ru>
2024-01-13 15:49:45 +0300

    new settings

--- posh_profile/Microsoft.PowerShell_profile.ps1
+++ posh_profile/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,8 @@ $env:Path = $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Communit
     + ";~\bin\" `
     + ";C:\Program Files\JetBrains\JetBrains Rider 2022.1.2\jbr\bin" `
     + ";C:\Program Files\TF" `
-    + ";C:\Program Files\JetBrains\JetBrains Rider 2022.1.2\bin"
+    + ";C:\Program Files\JetBrains\JetBrains Rider 2022.1.2\bin" `
+    + ";C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer"
 
 Import-Module posh-git
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
