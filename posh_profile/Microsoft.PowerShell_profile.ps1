8e781099aa70c99c4ef2416ae19ab5da74865dcf
Zubik Dmitriy <zubik@eremex.ru>
2025-03-18 12:26:33 +0300

    fix

--- posh_profile/Microsoft.PowerShell_profile.ps1
+++ posh_profile/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 $env:Path = $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin" `
     + ";C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\" `
     + ";C:\Program Files\OpenSSL\bin\" `
@@ -12,30 +11,10 @@ Import-Module posh-git
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
 Import-Module Helpers
 Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -PredictionSource History
+
 #Set-PoshPrompt -Theme jandedobbeleer
 
 Set-Alias pbcopy Set-Clipboard
 Set-Alias pbpaste Get-Clipboard
 Set-Alias rungbdpi 'D:\zapret-discord-youtube-1.6.1\general (ALT2).bat'
-Set-Variable ddSource C:\Projects\ECAD\Main\Source\
-Set-Variable TF_AUTO_SAVE_CREDENTIALS 1
-=======
-$env:Path = $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin" `
-    + ";C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\" `
-    + ";C:\Program Files\OpenSSL\bin\" `
-    + ";~\bin\" `
-    + ";C:\Program Files\JetBrains\JetBrains Rider 2022.1.2\jbr\bin" `
-    + ";C:\Program Files\TF" `
-    + ";C:\Program Files\JetBrains\JetBrains Rider 2022.1.2\bin"
-
-Import-Module posh-git
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
-Import-Module Helpers
-Set-PSReadLineOption -PredictionSource History
-#Set-PoshPrompt -Theme jandedobbeleer
-
-Set-Alias pbcopy Set-Clipboard
-Set-Alias pbpaste Get-Clipboard
-Set-Variable ddSource C:\Projects\ECAD\Main\Source\
-Set-Variable TF_AUTO_SAVE_CREDENTIALS 1
->>>>>>> Stashed changes
