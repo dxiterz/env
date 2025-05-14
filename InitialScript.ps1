e401718c516bd8e47d4ac4bca581ff2653596fe2
Zubik Dmitriy <zubik@eremex.ru>
2025-05-14 10:55:33 +0300

    add install font script

--- InitialScript.ps1
+++ InitialScript.ps1
@@ -1,12 +1,11 @@
+#install Fonts (Use nerdfont MESLO LGM)
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ([scriptblock]::Create((Invoke-WebRequest 'https://to.loredo.me/Install-NerdFont.ps1')))
+
 #install choco
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #install oh-my-posh
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
-
-#Set TF diff
-#$TF_DIFF_COMMAND = "$((Get-Command code).Source) --diff %1 %2"
-[System.Environment]::SetEnvironmentVariable("TF_DIFF_COMMAND", "$((Get-Command code).Source) --diff %1 %2")
 
 
 git config --global alias.logline "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
