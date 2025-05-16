b8de99dc55a0a62f281f353f65c4aeb0588b9250
Zubik Dmitriy <zubik@eremex.ru>
2025-05-16 11:23:29 +0300

    update rungdpi

--- posh_profile/Microsoft.PowerShell_profile.ps1
+++ posh_profile/Microsoft.PowerShell_profile.ps1
@@ -8,7 +8,7 @@ $env:Path = $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Communit
     + ";C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer"
 
 Import-Module posh-git
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "~\.oh-my-posh\themes\jandedobbeleer.omp.json" | Invoke-Expression
 Import-Module Helpers
 Import-Module -Name Terminal-Icons
 Set-PSReadLineOption -PredictionSource History
@@ -17,4 +17,4 @@ Set-PSReadLineOption -PredictionSource History
 
 Set-Alias pbcopy Set-Clipboard
 Set-Alias pbpaste Get-Clipboard
-Set-Alias rungbdpi 'D:\zapret-discord-youtube-1.6.1\general (ALT2).bat'
+Set-Alias rungbdpi 'D:\zapret-discord-youtube-1.6.4\general (ALT2).bat'
